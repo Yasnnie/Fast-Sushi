a8eeab180e2813b72dc640326a4e153315cdd6ee
Yasmin Carvalho <miojodepopoca@gmail.com>
2020-11-28 21:09:03 -0300

    Index

---
+++ php/login.php
@@ -0,0 +1,39 @@
+<?php
+include("conexao.php");
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/login.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div id="box">
+        
+        <h1>Faça seu Login</h1>
+        <input type="text">
+        <br>
+        <input type="password" name="" id="">
+        <br>
+        <button>Entrar</button>
+
+        <p>Não tem um login? <a href="">Cadastre-se</a></p>
+    
+
+    </div>
+
+
+
+</body>
+</html>
