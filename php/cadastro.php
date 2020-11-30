5fbee5e49236116c7df9ef734226f1da9ef81824
Yasmin Carvalho <miojodepopoca@gmail.com>
2020-11-29 22:59:40 -0300

    Cadastro 2

--- php/cadastro.php
+++ php/cadastro.php
@@ -16,18 +16,25 @@ include("conexao.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/cadastro.css">
+
     <title>Cadastro - Fast Sushi</title>
 </head>
 
-<body>
+<body onload="getEstados()">
 
     <div id="outer">
         <div id="middle">
             <div id="inner">
                 <ul>
                     <li>
-                        <label for="option1">Pessoa Física</label> <input type="radio" name="tipoDeConta" id="option1">
-                        <label for="option2">Estabelecimento</label> <input type="radio" name="tipoDeConta" id="option2">
+                        <label for="option1" class="containerRadio">Pessoa Física:
+                            <input type="radio" name="tipoDeConta" id="option1">
+                            <span class="circle"></span>
+                        </label>
+                        <label for="option2" class="containerRadio">Estabelecimento:
+                            <input type="radio" name="tipoDeConta" id="option2">
+                            <span class="circle"></span>
+                        </label>
 
                     </li>
                     <li>
@@ -38,17 +45,20 @@ include("conexao.php");
                         <input type="text" name="" id="email" placeholder="E-mail">
                     </li>
                     <li>
+                        <input type="text" name="" id="email" placeholder="CPF">
+                    </li>
+                    <li>
                         <input type="password" name="" id="senha" placeholder="Senha">
                     </li>
                     <li>
                         <input type="password" name="" id="confsenha" placeholder="Confirme a Senha">
                     </li>
                     <li>
-                        <label for="">Estado:</label> 
-                        <select name="" id= "labelEstados">   
-
-                        </select>
+                        <label for="">Estado:</label>
+                        <select name="" id="labelEstados" onchange="getCidades()"></select>
                     </li>
+                    <label for="">Cidade:</label>
+                    <select name="" id="labelCidade"></select>
                     <li>
                         <input type="text" name="" id="" placeholder="Rua"> <input type="text" name="" id="numeroR" placeholder="Nº">
                     </li>
@@ -61,17 +71,11 @@ include("conexao.php");
             </div>
         </div>
     </div>
+    <script src="../js/pluginIBGE.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
-    <script type="text/javascript">                                   
-        $(document).ready( function() { 
-            try{
-                $('#labelEstados').load('cadastro.php');
-            } catch(e) {
-            }            
-            getEstados();
-        }); 
 
-</script> 
+    </script>
 </body>
 
 </html>
