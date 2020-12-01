22e4677ba6c01f4b221d7cdd9bb90162798c5a5b
Yasmin Carvalho <miojodepopoca@gmail.com>
2020-12-01 09:43:49 -0300

    Overview 2

--- php/overview.php
+++ php/overview.php
@@ -18,10 +18,18 @@
     <header>
         <img src="../img/logo.png" alt="">
         <div class="procurar">
-            <i class="fa fa-search" aria-hidden="true"></i>
-            <input type="text" name="" id="spaceprocurar" placeholder="Buscar...">
+            <form action="">
+                 <i class="fa fa-search" aria-hidden="true"></i>
+                 <input type="text" name="" id="spaceprocurar" placeholder="Buscar..." list="historico">
+                 <datalist id="historico"> 
+                     <option value="Chine Box"></option>
+                     <option value="Restaurante"></option>
+                 </datalist>
+            </form>
+     
+            
         </div>
-
+    
         <div class="barinfo">
             <a href="" class="user"> Usuário</a>
         </div>
@@ -62,11 +70,40 @@
                             </div>
 
                         </div>
-                        <p>Descrição de estabelecimento</p>
+                        <p>O Chinatown tem como visão vir a ser a melhor operação de culinária 
+                            chinesa no Brasil, portanto, em 1995, iniciou o sistema de franquias</p>
+
                     </div>
-                    <i class="fa fa-clock-o" aria-hidden="true"></i><label for="">12h~21</label>
+
+                    <div class="hours">
+                        <i class="fa fa-clock-o" aria-hidden="true"></i><label for="">12h~21</label>
+                    </div>
+
                 </li>
                 <li>
+                    <img src="../img/login.png" alt="">
+                    <div class="descEstabelecimento">
+                        <a href="">Nome do estabelecimento</a>
+                        <h3>Endereço:Acre/ Acrelandia/ Rua da casa amarela Nº 1000</h3>
+
+                        <div class="phone">
+                            <i class="fa fa-phone , iconphone" aria-hidden="true"></i>
+                            <label for="">Telefone: (09)40028922 </label>
+                            <div class="avalicao">
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                            </div>
+
+                        </div>
+                        <p>Descrição de estabelecimento</p>
+
+                    </div>
+                    <div class="hours">
+                        <i class="fa fa-clock-o" aria-hidden="true"></i><label for="">12h~21</label>
+                    </div>
 
                 </li>
             </ul>
