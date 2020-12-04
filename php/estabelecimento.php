3f06a778572461f0c7c9926b3646bfef63180927
Yasmin Carvalho <miojodepopoca@gmail.com>
2020-12-03 23:45:54 -0300

    Estabelecimento Page 4

--- php/estabelecimento.php
+++ php/estabelecimento.php
@@ -23,95 +23,135 @@
     <div id="box">
         <div class="container">
             <img src="../img/teste.jpg" alt="">
-
-
-
             <button><a href="">Histórico de Pedidos</a></button>
             <button>Editar Perfil</button>
+            <button>Cardápio</button>
             <button>Sair</button>
 
+        </div>
 
-            <div class="editar-perfil">
+        <div class="pedido">
+            <table>
+                <tr>
+                    <td>
+                        <h1>PED 001</h1>
+                        <h3>Yasmin Carvalho</h3>
+
+                        <a href="">Entregar</a>
+                    </td>
+                    <td>
+                        <h1>PED 001</h1>
+                        <h3>Yasmin Carvalho</h3>
+
+                        <a href="">Entregar</a>
+                    </td>
+                    <td>
+                        <h1>PED 001</h1>
+                        <h3>Yasmin Carvalho</h3>
+
+                        <a href="">Entregar</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h1>PED 001</h1>
+                        <h3>Yasmin Carvalho</h3>
+
+                        <a href="">Entregar</a>
+                    </td>
+                    <td>
+                        <h1>PED 001</h1>
+                        <h3>Yasmin Carvalho</h3>
+
+                        <a href="">Entregar</a>
+                    </td>
+                    <td>
+                        <h1>PED 001</h1>
+                        <h3>Yasmin Carvalho</h3>
+
+                        <a href="">Entregar</a>
+                    </td>
+
+                </tr>
 
 
-                <input type="text" placeholder="Nome">
-                <br>
-                <input type="text" placeholder="Telefone">
-                <br>
-                <h5>Horário de Funcionamento</h5>
-                <label for=""> Início:</label>
-                <input type="time" name="" id="">
-                <br>
-                <label for=""> Final:</label>
-                <input type="time" name="" id="">
-
-                <br>
-                <textarea name="" id="" maxlength="180" placeholder="Descrição"></textarea>
-
-
-            </div>
+            </table>
         </div>
 
 
-        <div class="container2">
-            <div class="cardapio">
-                <input type="file" name="" id="">
-                <input type="text" placeholder="Nome">
-                <textarea name="" id="" maxlength="200" placeholder="Descrição"></textarea>
-                <button>Adicionar ao Cardapio</button>
-            </div>
-
-            <div class="pedido">
-                <table>
-                    <tr>
-                        <td>
-                            <h1>PED 001</h1>
-                            <h3>Yasmin Carvalho</h3>
-
-                            <a href="">Entregar</a>
-                        </td>
-                        <td>
-                            <h1>PED 001</h1>
-                            <h3>Yasmin Carvalho</h3>
-
-                            <a href="">Entregar</a>
-                        </td>
-                        <td>
-                            <h1>PED 001</h1>
-                            <h3>Yasmin Carvalho</h3>
-
-                            <a href="">Entregar</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <h1>PED 001</h1>
-                            <h3>Yasmin Carvalho</h3>
-
-                            <a href="">Entregar</a>
-                        </td>
-                        <td>
-                            <h1>PED 001</h1>
-                            <h3>Yasmin Carvalho</h3>
-
-                            <a href="">Entregar</a>
-                        </td>
-                        <td>
-                            <h1>PED 001</h1>
-                            <h3>Yasmin Carvalho</h3>
-
-                            <a href="">Entregar</a>
-                        </td>
-
-                    </tr>
-
-
-                </table>
-            </div>
-
-        </div>
     </div>
 
+    <div class="cardapio">
+        <div class="categoria">
+            <form action="">
+                <h1>Criar Nova Categoria:</h1>
+                <label for="">Nome da Categoria:</label>
+                <br>
+                <input type="text">
+                <br>
+                <button>Adicionar ao Cardapio</button>
+            </form>
+        </div>
+        <hr>
+        <div class="prato">
+            <form action="" method="post">
+                <h1>Adicionar um prato novo:</h1>
+                <!--<input type="file" name="" id="">-->
+                <label for="">Nome do Prato:</label>
+                <br>
+                <input type="text" placeholder="">
+                <br>
+
+                <label for="">Selecione uma categoria:</label>
+                <div class="select-box">
+                    <select name="" id="">
+                        <option value="">Selecione</option>
+                        <option value="">Option 1</option>
+                    </select>
+                </div>
+
+                <label for="">Descrição:</label>
+                <br>
+                <textarea name="" id="" maxlength="200"></textarea>
+                <br>
+                <button>Adicionar ao Cardapio</button>
+            </form>
+        </div>
+
+    </div>
+
+    <div class="editar-perfil">
+        <form action="" method="post">
+            <label for="">Nome do Prato:</label>
+            <br>
+            <input type="text" >
+            <br>
+            <label for="">Telefone:</label>
+            <br>
+            <input type="text" placeholder="Telefone">
+            <br>
+            <label for="">Horário de Funcionamento</label>
+            <br>
+            <label for=""> Início:</label>
+            <input type="time" name="" id="">
+            <br>
+            <label for=""> Final:</label>
+            <input type="time" name="" id="">
+            <br>
+            <label for="">Descrição:</label>
+            <br>
+            <textarea name="" id="" maxlength="180" placeholder="Descrição"></textarea>
+            <br>
+            <button>Salvar</button>
+
+        </form>
+
+
+    </div>
+
+    <div class="historico-de-pedidos">
+
+    </div>
 
 
 
